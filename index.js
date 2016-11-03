2af16e717dba326b125790c400ae44e65360c91f
Crispioso <crispin.merriman@gmail.com>
2016-11-03 10:22:13 +0000

    Add search in topic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var main = document.getElementById('main'),
-    children = document.getElementById('children');
-parents = document.getElementById('parents');
+    $search = $('#search'),
+    $searchClear = $('#search-clear'),
+    parents = document.getElementById('parents');
 
 // Remove existing hash on load if present
 if (window.location.hash) {
@@ -12,17 +13,46 @@ fetch('taxonomy.json').then(function (response) {
     return response.json();
 }).then(function (responseJSON) {
     buildTaxonomy(responseJSON);
+    displayDataForActive();
+    bindSearch();
 });
 
 /* State - store what is active in each layer and what url's data is being displayed */
 var state = {
     displayedDataUrl: "",
-    displayedDataTitle: "",
+    displayedDataTitle: "all topics",
     noOfPagesOfData: 0,
     currentDataListPage: 0,
     numberOfResults: 0,
+    query: "",
     taxonomy: []
 };
+
+/* Bind search */
+function bindSearch() {
+    $search.submit(function(event) {
+        event.preventDefault();
+        state.query = $('#search-input').val();
+        displayDataForActive();
+        toggleClearButton();
+        bindSearchClear()
+    });
+
+    function toggleClearButton() {
+        if (state.query !== "") {
+            $searchClear.show();
+        } else {
+            $searchClear.hide();
+        }
+    }
+
+    function bindSearchClear() {
+        $searchClear.click(function() {
+            $("#search-input").val("");
+            state.query = "";
+        })
+    }
+}
 
 /* Build taxonomy into page and bind all events and view logic */
 function buildTaxonomy(taxonomy) {
@@ -81,36 +111,10 @@ function bindClicks() {
             state.displayedDataTitle = event.target.textContent;
 
             setActiveLinks(event.target);
-
+            setActiveTopicOnSearch();
             displayDataForActive();
         }
     });
-
-    function displayDataForActive() {
-        var dataList = document.getElementById('data-list'),
-            loadingAnimation = document.createElement('div');
-
-        loadingAnimation.className = 'loading';
-        emptyDataList();
-        dataList.appendChild(loadingAnimation);
-
-        fetch('https://www.ons.gov.uk' + state.displayedDataUrl + '/dataList/data?size=25').then(function (response) {
-            return response.json();
-        }).then(function (responseJSON) {
-            state.numberOfResults = responseJSON.result.numberOfResults;
-            state.currentDataListPage = 1;
-            var listHTML = buildListOfData(responseJSON);
-            state.noOfPagesOfData = responseJSON.result.paginator ? responseJSON.result.paginator.numberOfPages : 1;
-            emptyDataList();
-            dataList.appendChild(listHTML);
-        });
-
-        function emptyDataList() {
-            while (dataList.firstChild) {
-                dataList.removeChild(dataList.firstChild);
-            }
-        }
-    }
 
     function setActiveLinks(activeLink) {
         var $oldActive = $('.active'),
@@ -126,6 +130,37 @@ function bindClicks() {
             $activeItem.parents('ul').addClass('active');
         }
     }
+
+    function setActiveTopicOnSearch() {
+        var $searchTopic = $search.find('#search-topic');
+        $searchTopic.html('"' + state.displayedDataTitle + '"');
+    }
+}
+
+function displayDataForActive() {
+    var dataList = document.getElementById('data-list'),
+        loadingAnimation = document.createElement('div');
+
+    loadingAnimation.className = 'loading';
+    emptyDataList();
+    dataList.appendChild(loadingAnimation);
+
+    fetch('https://www.ons.gov.uk' + state.displayedDataUrl + '/dataList/data?size=25' + '&query=' + state.query).then(function (response) {
+        return response.json();
+    }).then(function (responseJSON) {
+        state.numberOfResults = responseJSON.result.numberOfResults;
+        state.currentDataListPage = 1;
+        var listHTML = buildListOfData(responseJSON);
+        state.noOfPagesOfData = responseJSON.result.paginator ? responseJSON.result.paginator.numberOfPages : 1;
+        emptyDataList();
+        dataList.appendChild(listHTML);
+    });
+
+    function emptyDataList() {
+        while (dataList.firstChild) {
+            dataList.removeChild(dataList.firstChild);
+        }
+    }
 }
 
 function buildListOfData(listJSON) {
@@ -136,29 +171,31 @@ function buildListOfData(listJSON) {
     }
 
     var listHTML = document.createElement('ul'),
-        tempHTMLArray = [],
-        requests = [];
+        tempHTMLArray = [];
+
+    if (listJSON.result.results.length === 0) {
+        listHTML.innerHTML = "No results available for '" + state.query + "' in " + state.displayedDataTitle;
+        return listHTML
+    }
 
     $(listJSON.result.results).each(function () {
-        var id = (Math.floor(Math.random() * 90000) + 10000) + this.uri;
+        var id = (Math.floor(Math.random() * 90000) + 10000) + this.uri,
+            metadata = $(this)[0].description;
 
-        requests.push(resolveDataMetadata(this.uri, success = function(metadata) {
-            var thisReleaseDate = new Date(metadata.releaseDate);
-            thisReleaseDate = thisReleaseDate.getDate() + "/" + thisReleaseDate.getMonth().toString() + "/" + thisReleaseDate.getFullYear();
-            var thisLink = '<a target="_blank" href="https://www.ons.gov.uk' + metadata.uri + '">' + metadata.title + '</a>';
-            thisReleaseDate = '<span class="data__date">' + thisReleaseDate + '</span>';
-            var thisDescription = '<span class="data__description">' + metadata.description + '</span>';
+        // Build metadata
+        var thisReleaseDate = new Date(metadata.releaseDate);
+        thisReleaseDate = thisReleaseDate.getDate() + "/" + thisReleaseDate.getMonth().toString() + "/" + thisReleaseDate.getFullYear();
+        var thisLink = '<a target="_blank" href="https://www.ons.gov.uk' + $(this).uri + '">' + metadata.title + '</a>';
+        thisReleaseDate = '<span class="data__date">' + thisReleaseDate + '</span>';
+        var thisDescription = '<span class="data__description">' + metadata.summary + '</span>';
+        var metadataHTML = thisLink + thisReleaseDate + thisDescription;
 
-            var thisHTML = $(thisLink + thisReleaseDate + thisDescription);
-            $('li[data-id="' + id + '"]').html(thisHTML);
-        }));
-        var itemHTML = '<li class="data__item" data-id="' + id + '"></li>';
+        // Build whole list item
+        var itemHTML = '<li class="data__item" data-id="' + id + '">' + metadataHTML + '</li>';
         tempHTMLArray.push(itemHTML);
     });
 
-    $.when.apply($, requests).done(function() {
-        renderNextPageOnScroll();
-    });
+    renderNextPageOnScroll();
 
     if (!($('.data__heading').length)) {
         tempHTMLArray.unshift("<h2 class='data__heading'>Data available for '" + state.displayedDataTitle + "'</h2><p class='data__count'>" + state.numberOfResults + " results</p>");
@@ -167,21 +204,6 @@ function buildListOfData(listJSON) {
     listHTML.className = "data__list";
     listHTML.innerHTML = tempHTMLArray.join('');
     return listHTML;
-}
-
-function resolveDataMetadata(uri, success) {
-    return $.ajax({
-        url: "https://www.ons.gov.uk" + uri + "/data",
-        success: function (response) {
-            var metadataObject = {};
-            metadataObject.title = response.description.title;
-            metadataObject.releaseDate = response.description.releaseDate;
-            metadataObject.uri = response.uri;
-            metadataObject.description = response.description.summary ? response.description.summary : "No description available";
-            metadataObject.type = response.type;
-            success(metadataObject);
-        }
-    })
 }
 
 function renderNextPageOnScroll() {
@@ -197,9 +219,7 @@ function renderNextPageOnScroll() {
 
         // End of the document reached?
         if ($(document).height() - win.height() == win.scrollTop()) {
-            console.log("Current page: \n", state.currentDataListPage);
-            console.log("Total no. of pages: \n", state.noOfPagesOfData);
-            console.log('------');
+            win.off('scroll');
             if (state.currentDataListPage == state.noOfPagesOfData) {
                 console.log('last page');
                 $dataList.append('<p style="font-weight: bold; margin-left: 8px;">-- End of results --</p>');
@@ -217,6 +237,7 @@ function renderNextPageOnScroll() {
                 state.currentDataListPage++;
                 $('.loading').remove();
                 $dataList.append(listHTML);
+                renderNextPageOnScroll();
             });
 
         }
